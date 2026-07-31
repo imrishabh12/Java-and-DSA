d8338b0964a64c8ebf4f99e7b715801cdaf62139
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-07-31 12:10:16 +0530

    forloops

--- src/loops/for_loop.java
+++ src/loops/for_loop.java
@@ -130,7 +130,7 @@ public class for_loop {
 
 
 //Q6))Write a Java program to print the multiplication table of a given number from 1 to 10.
-
+/*
 package loops;
 import java.util.*;
 
@@ -145,10 +145,99 @@ public class for_loop {
 }
 
 
+ */
 
 
 
 
+
+
+//Q7)Write a Java program to calculate the factorial of a given number.
+/*
+package loops;
+import java.util.*;
+
+public class for_loop {
+    public static void main(String[] args) {
+        int fact = 1;
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+//Q8))Write a Java program to count how many numbers from 1 to 100 (both inclusive) are divisible by 3
+/*
+package loops;
+
+public class for_loop {
+    public static void main(String[] args){
+        int count =0;
+        for(int i=1;i<=100; i++){
+            if (i%3 == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Q9))Write a Java program to determine whether a given number is prime or not using a for loop
+package loops;
+import java.util.*;
+
+public class for_loop{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean isPrime = true;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+    }
+}
 
 
 
