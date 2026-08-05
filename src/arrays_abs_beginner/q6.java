cd54473afd76f4b759585a4e8532040fe76e6e50
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-05 13:25:27 +0530

    updating q6

--- src/arrays_abs_beginner/q6.java
+++ src/arrays_abs_beginner/q6.java
@@ -6,14 +6,19 @@
 //4. If the array is completely sorted, print "Array is sorted".
 
 package arrays_abs_beginner;
-
 public class q6 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 8, 5, 6};
-        for (int i = 0;i< arr.length;i++){
-            if (arr[i] < arr[i+1]){
-
+        boolean sorted = true;
+        for (int i = 0;i< arr.length-1;i++){
+            if (arr[i] > arr[i+1]){
+                System.out.println("we found first unsorted i.e, : " + arr[i]);
+                sorted = false;
+                break;
             }
         }
-
+        if(sorted){
+            System.out.println("array is sorted");
+        }
+    }
 }
