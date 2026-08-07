7235e5daf01b12854820727d1c4901fe630ade58
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-07 14:37:31 +0530

    q2

--- src/arrays3_manipulation/q1.java
+++ src/arrays3_manipulation/q1.java
@@ -1,15 +1,42 @@
+/*
+//reverse an array using simple tech
 package arrays3_manipulation;
-
-
 public class q1 {
-
     public static void main(String[] args) {
-
         int[] arr = {1, 2, 3, 4, 5};
-
-        // Traverse from last index to first index
         for (int i = arr.length - 1; i >= 0; i--) {
             System.out.println(arr[i]);
         }
+    }
+}
+
+ */
+
+//reverse an array using swapping
+//-->in this we will use 2 pointer approach
+
+package arrays3_manipulation;
+public class q1 {
+
+
+    public static void reverseArray(int arr[]){
+        int n= arr.length;
+        int i=0;
+        int j=n-1;
+
+        while(i < j){//will swap,i++, j--
+            int temp=arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+
+        }
+        for (i=0; i<n ;i++){                      /// or for(int k : arr){
+            System.out.println(arr[i]);           ///sout(k);
+        }
+    }public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        reverseArray(arr);
     }
 }
--- src/arrays3_manipulation/q2.java
+++ src/arrays3_manipulation/q2.java
@@ -1,4 +1,30 @@
-package arrays3_manipulation;
+//Q)) Right shift array by 1 position
+//not we cant move left to right bcz we will loose i+1 value
 
+
+package arrays3_manipulation;
 public class q2 {
+    static void shiftby1(int[] arr){
+        //step1-> store last value in temp
+        //s2--> shift  value
+        //s3--> now put temp value at idx 0
+        int n= arr.length;
+        int temp = arr[n-1]; //s1
+        for (int i=n-1; i>0; i--){//s2
+            arr[i] = arr[i-1];
+        }
+        arr[0]= temp;
+
+
+    }
+
+
+    public static void main(String[] args){
+        int arr[] = {3,5,4,1,2};
+        shiftby1(arr);
+
+        for(int k:arr){
+            System.out.println(k + " ");
+        }
+    }
 }
