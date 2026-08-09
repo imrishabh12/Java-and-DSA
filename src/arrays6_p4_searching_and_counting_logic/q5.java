5a997247c874635b5e9fec693d43dd2b031ec323
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-09 15:52:24 +0530

    q5

--- src/arrays6_p4_searching_and_counting_logic/q5.java
+++ src/arrays6_p4_searching_and_counting_logic/q5.java
@@ -1,4 +1,32 @@
+//Q5-find the element that appears exactly once in an array.
 package arrays6_p4_searching_and_counting_logic;
-
+import java.util.Scanner;
 public class q5 {
+    static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter size : ");
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i<n;i++){
+            System.out.print("eneter elemets");
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i<n; i++){
+            boolean found =false;
+
+            for (int j = 0; j < n; j++) {
+
+                if (i != j && arr[i] == arr[j]) { ///very imp line
+                    found = true;
+                    break;
+                }
+
+            }
+            if (!found){
+                System.out.println("unqiue element is : ");
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
 }
--- src/arrays6_p4_searching_and_counting_logic/q6.java
+++ src/arrays6_p4_searching_and_counting_logic/q6.java
@@ -1,3 +1,4 @@
+// print all the elements that appear more than once in an array.
 package arrays6_p4_searching_and_counting_logic;
 
 public class q6 {
