98dab6977366b1ca5f9cc7e98b61171d802d1859
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-10 15:32:39 +0530

    q7

--- src/arrays6_p4_transformation_and_manipulation/q7.java
+++ src/arrays6_p4_transformation_and_manipulation/q7.java
@@ -1,4 +1,26 @@
+//Q7 – Rotate an array by one position to the left.
 package arrays6_p4_transformation_and_manipulation;
-
+import java.util.Scanner;
 public class q7 {
+    static void main(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0 ;i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int temp = arr[0];//s1)saved first elem
+
+        for (int i = 0; i<n-1; i++){
+            arr[i] = arr[i+1];//s2) sfifting left
+        }
+        arr[n-1] = temp;//s3)first elem to last
+
+
+
+
+        for (int i = 0; i<n; i++){
+            System.out.println(arr[i]);
+        }
+    }
 }
