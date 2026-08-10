29def49a130bc558534061110e084b78cf8bc5aa
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-10 15:40:30 +0530

    q8

--- src/arrays6_p4_transformation_and_manipulation/q8.java
+++ src/arrays6_p4_transformation_and_manipulation/q8.java
@@ -1,4 +1,27 @@
+//Q8 – Rotate an Array by One Position to the Right
 package arrays6_p4_transformation_and_manipulation;
-
+import java.util.Scanner;
 public class q8 {
+    static void main(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0 ;i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int temp = arr[n-1];//s1)saved last elem
+
+        for (int i = n-1; i>0; i--){
+            arr[i] = arr[i-1];//s2) sfifting right
+        }
+        arr[0] = temp;//s3)last elem to first
+
+
+
+
+        for (int i = 0; i<n; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
 }
--- src/arrays6_p4_transformation_and_manipulation/q9.java
+++ src/arrays6_p4_transformation_and_manipulation/q9.java
@@ -1,3 +1,4 @@
+//Q9 – Swap Alternate Elements
 package arrays6_p4_transformation_and_manipulation;
 
 public class q9 {
