5f2c61a66356d78b774611339a08ffb9c928f112
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-14 18:32:13 +0530

    q1

--- src/arrays1_basics/strings1_basics_syntax.java
+++ src/arrays1_basics/strings1_basics_syntax.java
@@ -2,8 +2,11 @@ package arrays1_basics;
 
 public class strings1_basics_syntax {
     static void main(){
-        String str1 = "Rishabh";
+        /*String str1 = "Rishabh";
         String str2 = "Kumar";
         System.out.println(str1 + " " + str2);
+
+         */
+        System.out.println("Rishabh" + " " + "Kumar");
     }
 }
