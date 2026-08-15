31232939e14634738c71634bc86b66eaede61505
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-15 13:57:14 +0530

    rishabh kamal

---
+++ src/Strings_basics/q2.java
@@ -0,0 +1,14 @@
+//done by rishabh kamal// never visit it again
+package Strings_basics;
+import java.util.Scanner;
+public class q2 {
+static void main() {
+    System.out.println("hello world");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter  1st number: ");
+    int n1 = sc.nextInt();
+    System.out.println("enter 2nd number: ");
+    int n2 = sc.nextInt();
+    System.out.println("Final sum is:" + n1 + n2);
+}
+}
