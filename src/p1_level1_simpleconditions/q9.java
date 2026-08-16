b4656a95742d1ff28e114b0a4c9a6cf80f4a80ad
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-16 16:10:11 +0530

    q9

--- src/p1_level1_simpleconditions/q9.java
+++ src/p1_level1_simpleconditions/q9.java
@@ -1,4 +1,24 @@
+// Q9 - Take a character and check if it’s a vowel or consonant.
+
 package p1_level1_simpleconditions;
 
+import java.util.Scanner;
+
 public class q9 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+
+            System.out.println("Vowel");
+
+        } else {
+            System.out.println("Consonant");
+        }
+    }
 }
