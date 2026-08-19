f0e216c2ae0f43dc3fefd044affc3dca3caa2429
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-19 12:17:27 +0530

    1st question of level4

---
+++ src/p1_level4_logicaloperators_and_compoundstatements/q1.java
@@ -0,0 +1,23 @@
+// Q1 - Take a character and check if it is a letter, a digit, or neither.
+
+package p1_level4_logicaloperators_and_compoundstatements;
+
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            System.out.println("Letter");
+        } else if (ch >= '0' && ch <= '9') {
+            System.out.println("Digit");
+        } else {
+            System.out.println("Neither");
+        }
+    }
+}
