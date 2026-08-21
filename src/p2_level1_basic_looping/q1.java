0933313522d9b94d3acfca121aa11491cc542ff8
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-21 12:52:09 +0530

    1st question of phase2

---
+++ src/p2_level1_basic_looping/q1.java
@@ -0,0 +1,12 @@
+// Q1 - Print numbers from 1 to 10.
+
+package p2_level1_basic_looping;
+
+public class q1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
