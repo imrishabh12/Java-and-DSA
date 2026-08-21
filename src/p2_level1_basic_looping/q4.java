2c332c2bd3300a28bef5322a80948110e53d19fc
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-21 12:54:29 +0530

    q4

---
+++ src/p2_level1_basic_looping/q4.java
@@ -0,0 +1,12 @@
+// Q4 - Print numbers from 10 down to 1.
+
+package p2_level1_basic_looping;
+
+public class q4 {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
