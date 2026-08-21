6e7705f869bf31cdf9d05a76b6b30fd31163f5a7
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-21 12:56:57 +0530

    q9

---
+++ src/p2_level1_basic_looping/q9.java
@@ -0,0 +1,23 @@
+// Q9 - Print the factorial of a given number.
+
+package p2_level1_basic_looping;
+
+import java.util.Scanner;
+
+public class q9 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial = " + factorial);
+    }
+}
