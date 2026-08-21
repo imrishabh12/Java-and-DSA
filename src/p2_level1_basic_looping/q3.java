b30a906ac07d52ab64cbbb7d70e9a9f20a79475a
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-21 12:54:06 +0530

    q3

---
+++ src/p2_level1_basic_looping/q3.java
@@ -0,0 +1,12 @@
+// Q3 - Print all odd numbers between 1 and 100.
+
+package p2_level1_basic_looping;
+
+public class q3 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
