294e87aa7bb9af05c16e646c478b16658af98297
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-23 01:06:09 +0530

    q5

---
+++ src/p2_level3_mathematical_and_logical_patterns/q5.java
@@ -0,0 +1,37 @@
+// Q5 - Find LCM of two numbers using loops.
+
+package p2_level3_mathematical_and_logical_patterns;
+
+import java.util.Scanner;
+
+public class q5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int lcm;
+
+        if (a > b) {
+            lcm = a;
+        } else {
+            lcm = b;
+        }
+
+        while (true) {
+
+            if (lcm % a == 0 && lcm % b == 0) {
+                break;
+            }
+
+            lcm++;
+        }
+
+        System.out.println("LCM = " + lcm);
+    }
+}
