82f9bac8ac1725b4ed9028547303af820ecc3f46
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-22 16:36:26 +0530

    q4

---
+++ src/p2_level2_number_based_looping_logic/q4.java
@@ -0,0 +1,28 @@
+// Q4 - Find the sum of digits of a number.
+
+package p2_level2_number_based_looping_logic;
+
+import java.util.Scanner;
+
+public class q4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        while (n != 0) {
+
+            int digit = n % 10;
+
+            sum = sum + digit;
+
+            n = n / 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+    }
+}
