11571ee6975f08f70e770faac4aa9375afaf4b45
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-24 12:50:20 +0530

    q4

--- src/arrays8_p4_logical_and_applied_arraysproblem/q4.java
+++ src/arrays8_p4_logical_and_applied_arraysproblem/q4.java
@@ -4,5 +4,22 @@ import java.util.Scanner;
 public class q4 {
     static void main(){
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int smallest = arr[0];
+        int secondSmallest = arr[0];
+        for (int i = 1; i<n; i++){
+            if (arr[i] < smallest){
+                secondSmallest = smallest;
+                smallest = arr[i];
+            } else if (arr[i] < secondSmallest && arr[i] != smallest) {//Because we want the second distinct smallest value
+                secondSmallest = arr[i];
+
+            }
+        }
+        System.out.println("second smallest element is : " + secondSmallest);
     }
 }
